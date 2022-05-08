2f7dacc5887d8f4d91f1dbc6d656ef31e349d4bf
RegosAdam <regosadam01@gmail.com>
2022-05-08 14:11:59 +0200

    first commit

---
+++ app/src/main/java/com/example/szegedi_menetrendek/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.szegedi_menetrendek;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getName();
+    private static final int SECRET_KEY = 99;
+
+    EditText emailET;
+    EditText passwordET;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        emailET = findViewById(R.id.txtEmail);
+        passwordET = findViewById(R.id.txtPwd);
+
+        Log.i(LOG_TAG, "onCreate");
+    }
+
+    public void login(View view) {
+        String email = emailET.getText().toString();
+        String password = passwordET.getText().toString();
+
+        Log.i(LOG_TAG, "Bejelentkezett: " + email + ", jelszó: " + password);
+    }
+
+    public void register(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        intent.putExtra("SECRET_KEY", SECRET_KEY);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(LOG_TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(LOG_TAG, "onRestart");
+    }
+}
---
+++ app/src/main/java/com/example/szegedi_menetrendek/RegisterActivity.java
@@ -0,0 +1,104 @@
+package com.example.szegedi_menetrendek;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class RegisterActivity extends AppCompatActivity {
+    private static final String LOG_TAG = RegisterActivity.class.getName();
+    private static final int SECRET_KEY = 99;
+
+    EditText userNameEditText;
+    EditText userEmailEditText;
+    EditText passwordEditText;
+    EditText passwordConfirmEditText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        // Bundle bundle = getIntent().getExtras();
+        // int secret_key = bundle.getInt("SECRET_KEY");
+        int secret_key = getIntent().getIntExtra("SECRET_KEY", 0);
+
+        if (secret_key != 99) {
+            finish();
+        }
+
+        userNameEditText = findViewById(R.id.txtName);
+        userEmailEditText = findViewById(R.id.txtEmail);
+        passwordEditText = findViewById(R.id.txtPwd);
+        passwordConfirmEditText = findViewById(R.id.txtPwdRe);
+
+        Log.i(LOG_TAG, "onCreate");
+    }
+
+    public void register(View view) {
+        String userName = userNameEditText.getText().toString();
+        String email = userEmailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        String passwordConfirm = passwordConfirmEditText.getText().toString();
+
+        if (!password.equals(passwordConfirm)) {
+            Log.e(LOG_TAG, "Nem egyenlő a jelszó és a megerősítése.");
+            return;
+        }
+
+        Log.i(LOG_TAG, "Regisztrált: " + userName + ", e-mail: " + email);
+        // TODO: A regisztrációs funkcionalitást meg kellene valósítani egyszer.
+        openTransports();
+    }
+
+    private void openTransports(/* userdata */){
+        Intent intent = new Intent(this, PublicTransportActivity.class);
+        intent.putExtra("SECRET_KEY", SECRET_KEY);
+        startActivity(intent);
+    }
+
+    public void cancel(View view) {
+        finish();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(LOG_TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(LOG_TAG, "onRestart");
+    }
+}
